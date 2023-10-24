fe2e42204b480b54b8261bf1e34917c4534945ad
Jonas Otto <jonas@jonasotto.com>
2023-10-24 13:07:33 +0200

    cleanup, formatting
    Signed-off-by: Jonas Otto <jonas@jonasotto.com>

--- drivers/comparator/comp_stm32.c
+++ drivers/comparator/comp_stm32.c
@@ -8,7 +8,6 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/pinctrl.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/sys/printk.h>
 
 #include <zephyr/dt-bindings/comparator/stm32.h>
 
@@ -20,15 +19,12 @@ BUILD_ASSERT(COMP_STM32_INVERTINGINPUT_1_2VREFINT == COMP_INVERTINGINPUT_1_2VREF
 BUILD_ASSERT(COMP_STM32_INVERTINGINPUT_3_4VREFINT == COMP_INVERTINGINPUT_3_4VREFINT, "");
 BUILD_ASSERT(COMP_STM32_INVERTINGINPUT_VREFINT == COMP_INVERTINGINPUT_VREFINT, "");
 BUILD_ASSERT(COMP_STM32_INVERTINGINPUT_DAC1 == COMP_INVERTINGINPUT_DAC1, "");
-BUILD_ASSERT(COMP_STM32_INVERTINGINPUT_DAC1SWITCHCLOSED == COMP_INVERTINGINPUT_DAC1SWITCHCLOSED,
-	     "");
+BUILD_ASSERT(COMP_STM32_INVERTINGINPUT_DAC1SWITCHCLOSED == COMP_INVERTINGINPUT_DAC1SWITCHCLOSED, "");
 BUILD_ASSERT(COMP_STM32_INVERTINGINPUT_DAC2 == COMP_INVERTINGINPUT_DAC2, "");
 BUILD_ASSERT(COMP_STM32_INVERTINGINPUT_IO1 == COMP_INVERTINGINPUT_IO1, "");
 
 BUILD_ASSERT(COMP_STM32_NONINVERTINGINPUT_IO1 == COMP_NONINVERTINGINPUT_IO1, "");
-BUILD_ASSERT(COMP_STM32_NONINVERTINGINPUT_DAC1SWITCHCLOSED ==
-		     COMP_NONINVERTINGINPUT_DAC1SWITCHCLOSED,
-	     "");
+BUILD_ASSERT(COMP_STM32_NONINVERTINGINPUT_DAC1SWITCHCLOSED == COMP_NONINVERTINGINPUT_DAC1SWITCHCLOSED, "");
 
 BUILD_ASSERT(COMP_STM32_OUTPUT_NONE == COMP_OUTPUT_NONE, "");
 BUILD_ASSERT(COMP_STM32_OUTPUT_TIM1BKIN == COMP_OUTPUT_TIM1BKIN, "");
@@ -39,10 +35,9 @@ BUILD_ASSERT(COMP_STM32_OUTPUT_TIM3IC1 == COMP_OUTPUT_TIM3IC1, "");
 BUILD_ASSERT(COMP_STM32_OUTPUT_TIM2OCREFCLR == COMP_OUTPUT_TIM2OCREFCLR, "");
 BUILD_ASSERT(COMP_STM32_OUTPUT_TIM3OCREFCLR == COMP_OUTPUT_TIM3OCREFCLR, "");
 
-LOG_LEVEL_SET(CONFIG_COMPARATOR_LOG_LEVEL);
+LOG_MODULE_REGISTER(CONFIG_COMPARATOR_LOG_LEVEL);
 
 struct stm32_comp_cfg {
-	LOG_INSTANCE_PTR_DECLARE(log);
 	const struct pinctrl_dev_config *pcfg;
 	COMP_TypeDef *instance;
 	uint32_t input_noninverting;
@@ -51,11 +46,9 @@ struct stm32_comp_cfg {
 	uint32_t output;
 };
 
-struct stm32_comp_data {
-};
+struct stm32_comp_data {};
 
-int stm32_comp_init(const struct device *dev)
-{
+int stm32_comp_init(const struct device *dev) {
 	const struct stm32_comp_cfg *cfg = dev->config;
 
 	COMP_HandleTypeDef hcomp;
@@ -64,8 +57,7 @@ int stm32_comp_init(const struct device *dev)
 	hcomp.Init.InvertingInput = cfg->input_inverting;
 	hcomp.Init.NonInvertingInput = cfg->input_noninverting;
 	hcomp.Init.Output = cfg->output;
-	hcomp.Init.OutputPol =
-		cfg->invert_output ? COMP_OUTPUTPOL_INVERTED : COMP_OUTPUTPOL_NONINVERTED;
+	hcomp.Init.OutputPol = cfg->invert_output ? COMP_OUTPUTPOL_INVERTED : COMP_OUTPUTPOL_NONINVERTED;
 	hcomp.Init.Hysteresis = COMP_HYSTERESIS_NONE;
 	hcomp.Init.Mode = COMP_MODE_HIGHSPEED;
 	hcomp.Init.WindowMode = COMP_WINDOWMODE_DISABLE;
@@ -74,56 +66,52 @@ int stm32_comp_init(const struct device *dev)
 	HAL_StatusTypeDef res = HAL_COMP_Init(&hcomp);
 
 	if (res != HAL_OK) {
-		LOG_INST_ERR(cfg->log, "HAL_COMP_Init returned error 0x%x", (int)res);
+		LOG_ERR("HAL_COMP_Init for %p error %d", (void *) cfg->instance, (int) res);
 		return -EIO;
 	}
 
 	res = HAL_COMP_Start(&hcomp);
 	if (res != HAL_OK) {
-		LOG_INST_ERR(cfg->log, "HAL_COMP_Start returned error 0x%x", (int)res);
+		LOG_ERR("HAL_COMP_Start for %p error %d", (void *) cfg->instance, (int) res);
 		return -EIO;
 	}
 
 	int err = pinctrl_apply_state(cfg->pcfg, PINCTRL_STATE_DEFAULT);
 
 	if (err != 0) {
-		LOG_INST_ERR(cfg->log, "pinctrl_apply_state returned error %d", err);
+		LOG_ERR("pinctrl_apply_state for %p error %d", (void *) cfg->instance, err);
 		return err;
 	}
-	LOG_INST_INF(cfg->log, "Init done!");
 
 	return 0;
 }
 
-#define COMP_STM32_INIT(inst)                                                                      \
-	LOG_INSTANCE_REGISTER(stm32_comp, inst, LOG_LEVEL_INF);                                    \
-	PINCTRL_DT_INST_DEFINE(inst);                                                              \
-	BUILD_ASSERT(IS_COMP_ALL_INSTANCE((COMP_TypeDef *)DT_INST_REG_ADDR(inst)),                 \
-		     "Invalid base address " STRINGIFY(DT_INST_REG_ADDR(             \
-                   inst)) " for comparator instance " STRINGIFY(inst) "!");              \
-	BUILD_ASSERT(IS_COMP_NONINVERTINGINPUT(DT_INST_PROP(inst, zephyr_input_noninverting)),     \
-		     "Invalid noninverting input " STRINGIFY(DT_INST_PROP(                    \
-          inst, zephyr_input_noninverting)) " for comparator "                  \
-							       "instance " STRINGIFY(inst) "!");   \
-	BUILD_ASSERT(IS_COMP_INVERTINGINPUT(DT_INST_PROP(inst, zephyr_input_inverting)),           \
-		     "Invalid inverting input " STRINGIFY(DT_INST_PROP(                       \
-          inst, zephyr_input_inverting)) " for comparator "                     \
-							    "instance " STRINGIFY(inst) "!");      \
-	BUILD_ASSERT(IS_COMP_OUTPUT(DT_INST_PROP_OR(inst, zephyr_output, COMP_OUTPUT_NONE)),       \
-		     "Invalid output " STRINGIFY(DT_INST_PROP_OR(                             \
-          inst, zephyr_output,                                                 \
-          COMP_OUTPUT_NONE)) " for comparator instance " STRINGIFY(inst) "!");  \
-	static const struct stm32_comp_cfg stm32_comp_cfg_##inst = {                               \
-		LOG_INSTANCE_PTR_INIT(log, stm32_comp, inst).pcfg =                                \
-			PINCTRL_DT_INST_DEV_CONFIG_GET(inst),                                      \
-		.instance = (COMP_TypeDef *)DT_INST_REG_ADDR(inst),                                \
-		.input_noninverting = DT_INST_PROP(inst, zephyr_input_noninverting),               \
-		.input_inverting = DT_INST_PROP(inst, zephyr_input_inverting),                     \
-		.invert_output = DT_INST_PROP(inst, zephyr_invert_output),                         \
-		.output = DT_INST_PROP_OR(inst, zephyr_output, COMP_OUTPUT_NONE),                  \
-	};                                                                                         \
-	static struct stm32_comp_data stm32_comp_data_##inst = {};                                 \
-	DEVICE_DT_INST_DEFINE(inst, stm32_comp_init, NULL, &stm32_comp_data_##inst,                \
-			      &stm32_comp_cfg_##inst, POST_KERNEL, 80, NULL);
+#define COMP_STM32_INIT(inst)                                                                                          \
+	PINCTRL_DT_INST_DEFINE(inst);                                                                                      \
+	BUILD_ASSERT(IS_COMP_ALL_INSTANCE((COMP_TypeDef *) DT_INST_REG_ADDR(inst)),                                        \
+				 "Invalid base address " STRINGIFY(DT_INST_REG_ADDR(inst)) " for comparator instance " STRINGIFY(      \
+						 inst) "!");                                                                                   \
+	BUILD_ASSERT(IS_COMP_NONINVERTINGINPUT(DT_INST_PROP(inst, zephyr_input_noninverting)),                             \
+				 "Invalid noninverting input " STRINGIFY(                                                              \
+						 DT_INST_PROP(inst, zephyr_input_noninverting)) " for comparator "                             \
+																		"instance " STRINGIFY(inst) "!");              \
+	BUILD_ASSERT(IS_COMP_INVERTINGINPUT(DT_INST_PROP(inst, zephyr_input_inverting)),                                   \
+				 "Invalid inverting input " STRINGIFY(                                                                 \
+						 DT_INST_PROP(inst, zephyr_input_inverting)) " for comparator "                                \
+																	 "instance " STRINGIFY(inst) "!");                 \
+	BUILD_ASSERT(IS_COMP_OUTPUT(DT_INST_PROP_OR(inst, zephyr_output, COMP_OUTPUT_NONE)),                               \
+				 "Invalid output " STRINGIFY(DT_INST_PROP_OR(                                                          \
+						 inst, zephyr_output, COMP_OUTPUT_NONE)) " for comparator instance " STRINGIFY(inst) "!");     \
+	static const struct stm32_comp_cfg stm32_comp_cfg_##inst = {                                                       \
+			.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(inst),                                                              \
+			.instance = (COMP_TypeDef *) DT_INST_REG_ADDR(inst),                                                       \
+			.input_noninverting = DT_INST_PROP(inst, zephyr_input_noninverting),                                       \
+			.input_inverting = DT_INST_PROP(inst, zephyr_input_inverting),                                             \
+			.invert_output = DT_INST_PROP(inst, zephyr_invert_output),                                                 \
+			.output = DT_INST_PROP_OR(inst, zephyr_output, COMP_OUTPUT_NONE),                                          \
+	};                                                                                                                 \
+	static struct stm32_comp_data stm32_comp_data_##inst = {};                                                         \
+	DEVICE_DT_INST_DEFINE(inst, stm32_comp_init, NULL, &stm32_comp_data_##inst, &stm32_comp_cfg_##inst, POST_KERNEL,   \
+						  CONFIG_COMP_INIT_PRIORITY, NULL);
 
 DT_INST_FOREACH_STATUS_OKAY(COMP_STM32_INIT)
